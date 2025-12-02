6927152f993f79030efd2124388d5f34205d3548
Christophe Vellen <christophe.vellen@gmail.com>
2025-12-02 17:47:38 +0100

    add world map title

--- utils/world_map.py
+++ utils/world_map.py
@@ -7,14 +7,17 @@ def html_world_map(df, features):
     with open("../data/world_map.json") as f:
         wm = json.load(f)
 
-    html_map = '<!DOCTYPE html><html lang="fr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" href="css/carto.css" /><style type="text/css"></style></head><body><div id="container"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 -50 2000 1000" xml:space="preserve">'
+    html_map = '<!DOCTYPE html><html lang="fr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" href="css/carto.css" /><style type="text/css"></style></head><body><div id="container">'
+    html_map += f'<h1 style="text-align:center; font-size:24px;">Regroupements des pays suivant les variables : {", ".join(features)}. Année {str(list(set(df['year']))[0])}.</h1>'
+    html_map += '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 -50 2000 1000" xml:space="preserve">'
 
     error_code = []
+    # continent:
     for continent_code, continent_map in wm.items():
         html_map += f"<g class='{continent_code}'>\n"
 
+        # pays:
         for country_code, country_map in continent_map.items():
-
             try:
                 html_map += (
                     "<path "
@@ -32,12 +35,11 @@ def html_world_map(df, features):
 
         html_map += "</g>"
 
-        # legend
+    # légende :
     html_map += "<g style='transform:translate(150px, 700px);'>"
-    # for i, k in range(len(set(df["cluster_color"]))):
     for i in range(len(set(df["cluster"]))):
         html_map += f"<g style='transform:translate(0,{i*30}px);'> <rect x='0' y='0' width='60' height='20' fill='{couleurs_vives[i]}' stroke='#000'></rect><text x='120' y='20' style='font-size:20px;'>cluster: {i}</text> </g>"
-
+    html_map += f"<g style='transform:translate(0,{len(set(df["cluster"]))*30}px);'> <rect x='0' y='0' width='60' height='20' fill='#aaa' stroke='#000'></rect><text x='120' y='20' style='font-size:18px;'>Manque de données</text> </g>"
     html_map += "</g></svg></div></body><script src='iso_code.js'></script></html>"
 
     list_error = [code for code in set(error_code)]
